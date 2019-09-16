89af12145a54246e1d7830a259a935d53110d5f9
Javey <jiawei23716@sina.com>
2019-09-16 09:51:40 +0800

    add some test

--- src/app/button/index.ts
+++ src/app/button/index.ts
@@ -1,14 +1,21 @@
 import {NgModule, Component, ElementRef, ViewContainerRef, ContentChild, TemplateRef, ViewChild, ChangeDetectorRef} from '@angular/core';
-import Intact from 'intact';
+import Intact, {functionalWrapper} from 'intact';
 import {Button, ButtonGroup} from 'kpc/@css/components/button';
 import {Table, TableColumn} from 'kpc/@css/components/table';
+import {Dropdown, DropdownMenu, DropdownItem} from 'kpc/@css/components/dropdown';
+import {configure} from 'kpc/@css/components/utils';
 
-const components = [Button, ButtonGroup, Table, TableColumn];
+configure({useWrapper: true});
 
 Intact.decorate(Button, 'k-button');
 Intact.decorate(ButtonGroup, 'k-button-group');
 Intact.decorate(Table, 'k-table');
 Intact.decorate(TableColumn, 'k-table-column', ['template']);
+Intact.decorate(DropdownMenu, 'k-dropdown-menu');
+Intact.decorate(DropdownItem, 'k-dropdown-item');
+const DropdownWrapper = functionalWrapper(Dropdown, 'k-dropdown');
+
+const components = [Button, ButtonGroup, Table, TableColumn, DropdownWrapper, DropdownMenu, DropdownItem];
 
 @NgModule({
     declarations: components,
